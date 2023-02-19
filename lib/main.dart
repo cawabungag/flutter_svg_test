efacec8d625ba9778e81946dda2334b4a01919bf
Ilyas Ilyasov <38839317+cawabungag@users.noreply.github.com>
2023-02-19 19:00:59 +0300

    init comit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Flutter SVG Demo';
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final carouselSlider = CarouselSlider(
+      options: CarouselOptions(height: 400.0),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return SvgPicture.network(
+                'https://jovial.com/images/jupiter.svg',
+                placeholderBuilder: (BuildContext context)
+                => const CircularProgressIndicator());
+          },
+        );
+      }).toList(),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: carouselSlider,
+      ),
+    );
+  }
+}
